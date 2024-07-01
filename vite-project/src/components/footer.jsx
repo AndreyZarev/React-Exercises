6501f677c66efc0e1f8751de3146da354f2c3a2a
tocode <andrey.zarev@outlook.com>
2024-07-01 20:58:37 +0300

    Create footer.jsx

---
+++ vite-project/src/components/footer.jsx
@@ -0,0 +1,15 @@
+
+export default function Footer() {
+    return (
+
+        <footer>
+            <div>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis quo dignissimos dolor voluptate maxime placeat possimus corrupti repudiandae, saepe voluptates mollitia voluptatum est, delectus impedit debitis quis. Quod, beatae voluptatem.
+                </p>
+            </div>
+        </footer>
+    )
+
+
+}
