2eb8223553c6601c67aab598f3141f555a5a6168
tocode <andrey.zarev@outlook.com>
2024-07-01 20:58:41 +0300

    Create main.jsx

---
+++ vite-project/src/components/main.jsx
@@ -0,0 +1,11 @@
+export default function Main() {
+    return (<section>
+        <article>
+            <div>
+                <h1>Armwrestling</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus cumque magnam adipisci odit quas iste, possimus aliquam vitae reprehenderit expedita laboriosam ex, ducimus nemo numquam. Maxime dolor ipsam quisquam esse atque repellendus tenetur, ipsum sunt minima placeat, sapiente ab facere aperiam accusamus adipisci laborum ratione aliquam deserunt. Quas corrupti esse cumque ratione dolor facilis cum necessitatibus sint, ducimus, quis odit?</p>
+            </div>
+        </article>
+    </section>
+    )
+}
