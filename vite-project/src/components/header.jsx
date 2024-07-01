2c34115005608ab348ca439896558753ffb6def2
tocode <andrey.zarev@outlook.com>
2024-07-01 20:58:39 +0300

    Create header.jsx

---
+++ vite-project/src/components/header.jsx
@@ -0,0 +1,18 @@
+export default function Header() {
+
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>About</li>
+                    <li>Offer</li>
+                    <li>Register</li>
+                    <li>Login</li>
+                </ul>
+            </nav>
+        </header>
+    )
+
+}
+
