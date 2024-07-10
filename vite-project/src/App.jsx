74d95a172bbc5730e489147c55e3f07957068ae7
tocode <andrey.zarev@outlook.com>
2024-07-10 17:00:54 +0300

    Update App.jsx

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,15 +1,18 @@
 import GetMovies from "./components/main"
-
+import { useState } from "react"
+const [count, setCount] = useState(0)
 function App() {
 
   const movieList = [
     "Godfather",
     "Superman",
     "Batman",
-    "Baki"
+    "Baki",
+    count,
+    setCount
   ]
   return (
-
+   
     <GetMovies movieList={movieList} title="Great movies" />
 
   )
