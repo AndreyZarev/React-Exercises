0b21fb30e46885e081e72b0d7d0a765acd12f7b0
tocode <andrey.zarev@outlook.com>
2024-07-10 17:00:58 +0300

    Update main.jsx

--- vite-project/src/components/main.jsx
+++ vite-project/src/components/main.jsx
@@ -3,7 +3,7 @@ import MovieTitle from "./movieTitle"
 export default function getMovies(props) {
     return (
         <>
-            {console.log(props.movieList)}
+            
             <MovieTitle title={props.title} />
             <ul>
                 <li>{props.movieList[0]}</li>
